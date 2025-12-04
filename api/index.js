b064be07ead6d9c9145aafd6f98df2799d19a3db
Maffew110 <matheusbranco.bg009@academico.ifsul.edu.br>
2025-12-04 16:32:15 -0300

    vercel

---
+++ api/index.js
@@ -0,0 +1,239 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+import { createServer } from 'http';
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+// Caminho correto das views e public
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+// COLOCAR OS MODELS AQUI (colocar o caminho ../)
+
+import Funcionario from '../models/Funcionario.js';
+import Instrumento from '../models/Instrumento.js';
+import Cliente from '../models/Cliente.js';
+import Financa from '../models/Financa.js';
+
+//FIM MODELS
+
+// Servir arquivos estáticos
+//app.use(express.static(join(__dirname, '../public')));
+app.set('views', join(__dirname, '../views'));
+
+// Rotas
+import multer from 'multer';
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+// COLOCAR AS ROTAS AQUI
+
+app.get('/', (req, res) => {
+    res.render("index")
+})
+
+app.get('/instrumento/lst', async (req, res) => {
+    //busca os instrumentos no banco de dados
+    const instrumentos = await Instrumento.find()
+    res.render("instrumento/lst", {instrumentos:instrumentos})
+})
+
+//vem do form de pesquisa
+app.post('/instrumento/lst', async (req, res) => {
+    //busca os instrumentos no banco de dados
+    const pesquisa = req.body.pesquisa
+    const instrumentos = await Instrumento.find(
+        {nome:{$regex:pesquisa, 
+            $options: "i"
+        }}
+    )
+    res.render("instrumento/lst", {instrumentos:instrumentos})
+})
+
+app.post('/instrumento/add/ok', upload.single('foto'), async (req, res) => {
+    //grava no banco
+    Instrumento.create({
+        nome:req.body.nome,
+        familia:req.body.familia,
+        fabricante:req.body.fabricante,
+        data_fabricacao:req.body.data_fabricacao,
+        preco:req.body.preco,
+        foto:req.file.buffer
+    })
+    res.render("instrumento/addok")
+})
+
+app.get('/instrumento/add', (req, res) => {
+    res.render("instrumento/add")
+})
+
+app.post('/instrumento/edt/:id', async (req, res) => {
+    const id = req.params.id
+    await Instrumento.findByIdAndUpdate(id, req.body)
+    res.render("instrumento/edtok")
+})
+
+app.get('/instrumento/edt/:id', async (req, res) => {
+    const id = req.params.id
+    const instrumento = await Instrumento.findById(id)
+    res.render("instrumento/edt", {instrumento})
+})
+
+
+app.get('/instrumento/del/:id', async (req, res) => {
+    const id = req.params.id
+    await Instrumento.findByIdAndDelete(id)
+    res.redirect("/instrumento/lst")
+})
+
+app.get('/funcionarios/lst', async (req, res) => {
+    //busca os funcionários no banco de dados
+    const funcionarios = await Funcionario.find()
+    res.render("funcionarios/lst", {funcionarios:funcionarios})
+})
+
+//vem do form de pesquisa
+app.post('/funcionarios/lst', async (req, res) => {
+    //busca os funcionários no banco de dados
+    const pesquisa = req.body.pesquisa
+    const funcionarios = await Funcionario.find(
+        {nome:{$regex:pesquisa, 
+            $options: "i"
+        }}
+    )
+    res.render("funcionarios/lst", {funcionarios:funcionarios})
+})
+
+app.post('/funcionarios/add/ok', async (req, res) => {
+    //grava no banco
+    await Funcionario.create(req.body)
+    res.render("funcionarios/addok")
+})
+
+app.get('/funcionarios/add', (req, res) => {
+    res.render("funcionarios/add")
+})
+
+app.post('/funcionarios/edt/:id', async (req, res) => {
+    const id = req.params.id
+    await Funcionario.findByIdAndUpdate(id, req.body)
+    res.render("funcionarios/edtok")
+})
+
+app.get('/funcionarios/edt/:id', async (req, res) => {
+    const id = req.params.id
+    const funcionarios = await Funcionario.findById(id)
+    res.render("funcionarios/edt", {funcionarios})
+})
+
+app.get('/funcionarios/del/:id', async (req, res) => {
+    const id = req.params.id
+    await Funcionario.findByIdAndDelete(id)
+    res.redirect("/funcionarios/lst")
+})
+
+
+app.get('/clientes/lst', async (req, res) => {
+    //busca os clientes no banco de dados
+    const clientes = await Cliente.find()
+    res.render("clientes/lst", {clientes:clientes})
+})
+
+//vem do form de pesquisa
+app.post('/clientes/lst', async (req, res) => {
+    //busca os clientes no banco de dados
+    const pesquisa = req.body.pesquisa
+    const clientes = await Cliente.find(
+        {nome:{$regex:pesquisa, 
+            $options: "i"
+        }}
+    )
+    res.render("clientes/lst", {clientes:clientes})
+})
+
+app.post('/clientes/add/ok', async (req, res) => {
+    //grava no banco
+    await Cliente.create(req.body)
+    res.render("clientes/addok")
+})
+
+app.get('/clientes/add', (req, res) => {
+    res.render("clientes/add")
+})
+
+app.post('/clientes/edt/:id', async (req, res) => {
+    const id = req.params.id
+    await Cliente.findByIdAndUpdate(id, req.body)
+    res.render("clientes/edtok")
+})
+
+app.get('/clientes/edt/:id', async (req, res) => {
+    const id = req.params.id
+    const cliente = await Cliente.findById(id)
+    res.render("clientes/edt", {cliente})
+})
+
+app.get('/clientes/del/:id', async (req, res) => {
+    const id = req.params.id
+    await Cliente.findByIdAndDelete(id)
+    res.redirect("/clientes/lst")
+})
+
+
+app.get('/financas/lst', async (req, res) => {
+    const financas = await Financa.find()
+    res.render("financas/lst", {financas:financas})
+})
+
+//vem do form de pesquisa
+app.post('/financas/lst', async (req, res) => {
+    const pesquisa = req.body.pesquisa
+    const financas = await Financa.find(
+        {valor:{$regex:pesquisa, 
+            $options: "i"
+        }}
+    )
+    res.render("financas/lst", {financas:financas})
+})
+
+app.post('/financas/add/ok', async (req, res) => {
+    //grava no banco
+    await Financa.create(req.body)
+    res.render("financas/addok")
+})
+
+app.get('/financas/add', (req, res) => {
+    res.render("financas/add")
+})
+
+app.post('/financas/edt/:id', async (req, res) => {
+    const id = req.params.id
+    await Financa.findByIdAndUpdate(id, req.body)
+    res.render("financas/edtok")
+})
+
+app.get('/financas/edt/:id', async (req, res) => {
+    const id = req.params.id
+    const financas = await Financa.findById(id)
+    res.render("financas/edt", {financas})
+})
+
+app.get('/financas/del/:id', async (req, res) => {
+    const id = req.params.id
+    await Financa.findByIdAndDelete(id)
+    res.redirect("/financas/lst")
+})
+
+app.get('/site', (req, res) => {
+    res.render("site/index")
+})
+
+//FIM ROTAS
+app.listen(3001)
+// Exporta o handler compatível com Vercel
+export default app;
